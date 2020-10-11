1cf05c4dc6175cc9a73f35d662b8826521ce36ab
kang <minhyupp@gmail.com>
2020-10-12 06:28:29 +0900

    deploy

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,20 @@
 const mysql = require("mysql");
 
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "mysql",
+//   user: "root",
+//   password: "root",
+//   database: "myapp",
+// });
+
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "root",
-  database: "myapp",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_HOST,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 exports.pool = pool;
