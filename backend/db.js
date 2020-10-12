5ea7ae922ddf2da31622d8b07aed39a8916dd855
minhyup <minhyupp@gmail.com>
2020-10-12 11:28:21 +0900

    'commit'

--- backend/db.js
+++ backend/db.js
@@ -10,8 +10,8 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOST, //RDS 호스트
+  user: process.env.MYSQL_USER,
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT,
